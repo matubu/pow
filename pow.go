4254bae11de47127fcd8aced56d5f4488acf2cc3
matubu <78111467+matubu@users.noreply.github.com>
2022-01-06 08:52:49 +0100

    Update pow.go

--- pow.go
+++ pow.go
@@ -3,13 +3,13 @@ import "fmt"
 
 func pow(a int, b int) int {
 	for r := 1; true; {
-    if b <= 0 { return r }
-    b--
+		if b <= 0 { return r }
+		b--
 		r *= a
 	}
-  return 1
+	return 1
 }
 
 func main() {
-    fmt.Println(pow(2, 4))
+	fmt.Println(pow(2, 4))
 }
